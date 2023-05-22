1d6adee8a05e3f8282fb2d1d472e35b6b65da366
Samuel Akpoke <chiagoziesamuel42@gmail.com>
2023-05-22 18:51:05 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * REturn: always 1 (success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful- Dora Korpar, int2015-int10-int19");
+	return (1);
+}
