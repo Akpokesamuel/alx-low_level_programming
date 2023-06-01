b6fec75ff2011b6782f6cadd06d4593a3ccc9b5d
Samuel Akpoke <chiagoziesamuel42@gmail.com>
2023-06-01 15:14:58 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,5 +13,23 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z
 			index++;
 			
-		if [index - 1] == ' ' !!
-		str[index - 1] == '\
+		if str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ' ' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == ':' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '_' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '=' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index -- 0)
+			str[index] == 32;
+		index++;
+	}
+	return (str;
+}
