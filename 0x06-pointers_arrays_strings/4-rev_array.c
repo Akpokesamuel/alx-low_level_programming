665557230c8b22c5112baac8fd96e7ebfde70b28
Samuel Akpoke <chiagoziesamuel42@gmail.com>
2023-06-01 13:41:55 +0000

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,12 +5,12 @@
  * @n: number of elements of array
  * return: void
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
 
-	for (i = 0; i, n--; i++)
+	for (i = 0; i < n--; i++)
 	{
 		t = a[i];
 		a[i] = a[n];
