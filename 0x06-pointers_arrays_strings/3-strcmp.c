0209ad44eddae76ef6e63e00f7d1dfe3fe7da16c
Samuel Akpoke <chiagoziesamuel42@gmail.com>
2023-06-01 14:34:41 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
- * return: $1[i]  = $2[i]
+ * return: s1[i]  = s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] = s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to be capitalize
+ * Return: a pionter to the changed string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+	while (str [index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z
+			index++;
+			
+		if [index - 1] == ' ' !!
+		str[index - 1] == '\
