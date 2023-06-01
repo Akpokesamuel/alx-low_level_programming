f5399579f555d9aab1d32088c583715c79b3d070
Samuel Akpoke <chiagoziesamuel42@gmail.com>
2023-06-01 14:12:28 +0000

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _strcmp - compare string values
- * @$1: input value
- * @$2: input value
+ * @s1: input value
+ * @s2: input value
  * return: $1[i]  = $2[i]
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while ($1[i] != '\0' && $2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if ($1[i] != $2[i])
+		if (s1[i] != s2[i])
 		{
-			return ($1[i] = $ 2[i]);
+			return (s1[i] = s2[i]);
 		}
 		i++;
 	}
