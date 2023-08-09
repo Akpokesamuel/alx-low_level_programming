8860493c7c5a97af13964b14d7add530ff0861b9
Samuel Akpoke <samuelhandsome42@gmail.com>
2023-08-09 02:51:47 +0000

    commit for  static libraries

---
+++ 0x08-recursion/0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checkes for lower character
+ * @c: the character to check
+ * Rrturn: 1 if c is lower, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
