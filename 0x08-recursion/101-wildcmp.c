36254ac6158ba7d533291d0f7c8c996bcdf71d08
Samuel Akpoke <samuelhandsome42@gmail.com>
2023-09-29 19:24:12 +0000

    commit for 0x08 c recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,10 +16,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '*')
+	else if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-
 	return (0);
 }
